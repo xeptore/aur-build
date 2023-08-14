74834ae185a6e6c0e61b13c53b8e8ce23c310ae3
xeptore <29199390+xeptore@users.noreply.github.com>
2023-08-14 08:20:05 +0330

    merge docker-credentials-pass-bin package build
    Signed-off-by: xeptore <29199390+xeptore@users.noreply.github.com>

---
+++ docker-credentials-pass-bin/pkgbuild.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+if [[ ! -v VERSION_NUMBER ]]; then
+  echo "'VERSION_NUMBER' variable must be set in the environment." 1>&2
+  exit 1
+fi
+
+source_x86_64="https://github.com/docker/docker-credential-helpers/releases/download/v${VERSION_NUMBER}/docker-credential-pass-v${VERSION_NUMBER}.linux-amd64"
+license_file_url="https://raw.githubusercontent.com/docker/docker-credential-helpers/v${VERSION_NUMBER}/LICENSE"
+
+wget -q "https://github.com/docker/docker-credential-helpers/releases/download/v${VERSION_NUMBER}/checksums.txt" -O checksums.txt
+wget -q "$license_file_url" -O LICENSE
+
+sha256sums_x86_64="$(grep "docker-credential-pass-v${VERSION_NUMBER}.linux-amd64" checksums.txt | cut -d ' ' -f 1)"
+sha256sums_license="$(sha256sum LICENSE | cut -d ' ' -f 1)"
+pkgname='docker-credential-pass-bin'
+
+wget -q https://aur.archlinux.org/rpc/v5/info/${pkgname} --header='accept: application/json' -O pkg-info.json
+
+pkgrel="$(jq -r '.results[0].Version' pkg-info.json | sed -n 's/^.*-//p')"
+echo "Got pkgrel $pkgrel on the AUR."
+
+self_latest_version="$(jq -r '.results[0].Version' pkg-info.json | sed -n 's/-[[:digit:]]\+$//p')"
+echo "Got version $self_latest_version on the AUR."
+if [[ "$self_latest_version" != "${VERSION_NUMBER}" ]]; then
+  echo 'Resetting pkgrel...'
+  pkgrel='1'
+elif [[ ! -v NO_PKGREL_INCREMENT ]]; then
+  echo 'Incrementing pkgrel...'
+  pkgrel=$((pkgrel + 1))
+fi
+
+cat >PKGBUILD <<EOF
+# Maintainer: Joel Noyce Barnham <joelnbarnham@gmail.com>
+# Contributor: xeptore <hello [ at ] xeptore [ dot ] dev>
+
+pkgname=${pkgname}
+pkgver=${VERSION_NUMBER}
+pkgrel=${pkgrel}
+pkgdesc='Store docker credentials using the Standard Unix Password Manager (pass)'
+arch=('x86_64')
+url='https://github.com/docker/docker-credential-helpers'
+license=('MIT')
+depends=('pass')
+makedepends=()
+provides=("${pkgname%-bin}")
+conflicts=("${pkgname%-bin}")
+source_x86_64=(
+  'docker-credential-pass-v${VERSION_NUMBER}.linux-amd64::${source_x86_64}'
+  'LICENSE::${license_file_url}'
+)
+sha256sums_x86_64=(
+  '${sha256sums_x86_64}'
+  '${sha256sums_license}'
+)
+
+package() {
+  install -D -m 0755 "\${srcdir}/docker-credential-pass-v\${pkgver}.linux-amd64" "\${pkgdir}/usr/bin/docker-credential-pass"
+  install -D -m 0644 "\${srcdir}/LICENSE" "\${pkgdir}/usr/share/licenses/\${pkgname}/LICENSE"
+}
+EOF
--- readme.md
+++ readme.md
@@ -1,23 +1,11 @@
-# Hugo AUR Build
+# AUR Builds
 
-Builds the latest release of [Hugo](https://gohugo.io) static website generator on schedule, and publishes to the [AUR](https://aur.archlinux.org/).
+Home of build scripts for AUR packages I maintain.
 
-## Installation
+## Packages
 
 Packages are available for install on the AUR:
 
-- Standard Edition: [`gohugo-bin`](https://aur.archlinux.org/packages/gohugo-bin)
-- Extended Edition: [`gohugo-extended-bin`](https://aur.archlinux.org/packages/gohugo-extended-bin)
+- [Hugo](https://gohugo.io) Extended Edition: [`gohugo-extended-bin`](https://aur.archlinux.org/packages/gohugo-extended-bin)
 
-You can install it using any AUR helper you prefer, e.g., [`yay`](https://github.com/Jguer/yay).
-
-## Features
-
-- Automatically versioned, and published based on upstream release
-- Support for `x86_64`, and `aarch64` architectures
-- Installs command line completions for `zsh`, `bash`, and `fish`
-- Installs man pages
-
-## Limitations
-
-- Only builds, and publishes the latest stable (non-prerelease, and non-draft) release of the upstream Hugo project repository
+- [`docker-credentials-pass-bin`](https://aur.archlinux.org/packages/docker-credentials-pass-bin)
